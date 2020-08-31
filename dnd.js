9812c32bd89c80384bc931db6c611a8d9e00dd1e
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-08-30 23:33:03 -0700

    Styles the opacity of the element being dragged

--- dnd.js
+++ dnd.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';   
+      }
+      
+      function handleDragEnd(e) {
+        this.style.opacity = '1';
+      }
+      
+      let items = document.querySelectorAll('.container .box');
+      items.forEach(function(item) {
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragend', handleDragEnd, false);
+      });
   });
