c8afa6dae4098c75dc9fa4145f9466f11a0e7e06
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-08-31 21:53:29 -0700

    Styles the boxes further with additional visuals

--- dnd.js
+++ dnd.js
@@ -1,16 +1,36 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
     function handleDragStart(e) {
-        this.style.opacity = '0.4';   
+      this.style.opacity = '0.4';   
+    }
+    
+    function handleDragEnd(e) {
+      this.style.opacity = '1';
+    }
+    
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault();
       }
       
-      function handleDragEnd(e) {
-        this.style.opacity = '1';
-      }
-      
-      let items = document.querySelectorAll('.container .box');
-      items.forEach(function(item) {
-        item.addEventListener('dragstart', handleDragStart, false);
-        item.addEventListener('dragend', handleDragEnd, false);
-      });
+      return false;
+    }
+
+    function handleDragEnter(e) {
+      this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+      this.classList.remove('over');
+    }
+    
+    let items = document.querySelectorAll('.container .box');
+    items.forEach(function(item) {
+      item.addEventListener('dragstart', handleDragStart, false);
+      item.addEventListener('dragover', handleDragOver, false);
+      item.addEventListener('dragenter', handleDragEnter, false);
+      item.addEventListener('dragleave', handleDragLeave, false);
+      item.addEventListener('dragend', handleDragEnd, false);
+    });
+
   });
