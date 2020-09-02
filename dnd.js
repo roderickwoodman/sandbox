e0ccb007f1ceb7abfb5ef6f85e2f894d756e57f6
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-09-01 23:34:53 -0700

    Replaces boxes on mouse release

--- dnd.js
+++ dnd.js
@@ -1,36 +1,64 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    function handleDragStart(e) {
-      this.style.opacity = '0.4';   
-    }
+  var dragSrcEl = null;
+  
+  function handleDragStart(e) {
+    this.style.opacity = '0.4';
     
-    function handleDragEnd(e) {
-      this.style.opacity = '1';
-    }
-    
-    function handleDragOver(e) {
-      if (e.preventDefault) {
-        e.preventDefault();
-      }
-      
-      return false;
+    dragSrcEl = this;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
+
+  function handleDragOver(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
     }
 
-    function handleDragEnter(e) {
-      this.classList.add('over');
-    }
+    e.dataTransfer.dropEffect = 'move';
+    
+    return false;
+  }
 
-    function handleDragLeave(e) {
-      this.classList.remove('over');
+  function handleDragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove('over');
+  }
+
+  function handleDrop(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation(); // stops the browser from redirecting.
     }
     
-    let items = document.querySelectorAll('.container .box');
-    items.forEach(function(item) {
-      item.addEventListener('dragstart', handleDragStart, false);
-      item.addEventListener('dragover', handleDragOver, false);
-      item.addEventListener('dragenter', handleDragEnter, false);
-      item.addEventListener('dragleave', handleDragLeave, false);
-      item.addEventListener('dragend', handleDragEnd, false);
+    if (dragSrcEl != this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+    
+    return false;
+  }
+
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+    
+    items.forEach(function (item) {
+      item.classList.remove('over');
     });
-
+  }
+  
+  
+  let items = document.querySelectorAll('.container .box');
+  items.forEach(function(item) {
+    item.addEventListener('dragstart', handleDragStart, false);
+    item.addEventListener('dragenter', handleDragEnter, false);
+    item.addEventListener('dragover', handleDragOver, false);
+    item.addEventListener('dragleave', handleDragLeave, false);
+    item.addEventListener('drop', handleDrop, false);
+    item.addEventListener('dragend', handleDragEnd, false);
   });
+
+});
